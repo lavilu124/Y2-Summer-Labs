00e2f77fff462f2771d8e9bbbf2664f05da3771b
lavilu124 <127327520+lavilu124@users.noreply.github.com>
2024-07-15 07:31:54 +0300

    didnt finish spotify bonus

--- 0.2 Python Review/pythonReview.py
+++ 0.2 Python Review/pythonReview.py
@@ -1,3 +1,6 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
 def create_youtube_video(title, description, hashtag):
 	return {"title" : title, "description" : description, "likes" : 0, "dislikes" : 0, "comments" : {}, "hashtag" : hashtag[0:5]}
 
@@ -38,3 +41,19 @@ print(similarity_to_video(new_vid, new_vid2), "%")
 
 new_vid2["likes"] = 21
 print(is_trending(new_vid2))
+
+#bonus 2 (api)
+client_credentials_manager = SpotifyClientCredentials(client_id=id, client_secret= "SPOTIPY_CLIENT_SECRET")
+sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+
+#get top 10 songs 
+playlist_link = "https://open.spotify.com/playlist/37i9dQZEVXbNG2KDcFcKOF?si=1333723a6eff4b7f"
+playlist_URI = playlist_link.split("/")[-1].split("?")[0]
+track_uris = [x["track"]["uri"] for x in sp.playlist_tracks(playlist_URI)["items"]]
+
+songs_vid = []
+for i in range(10):
+	songs_vid.append(create_youtube_video(sp.playlist_tracks(playlist_URI)["items"][i]["track"]["name"], "yay", ["1", "2", "3", "4", "5", "6"]))
+
+for i in range(10):
+	print(i, ". ", songs_vid[i])
