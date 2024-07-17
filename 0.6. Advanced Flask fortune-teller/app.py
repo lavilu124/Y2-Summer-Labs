02da30007f092984d81ad848ecd8f5c4ec3e6e49
lavilu124 <127327520+lavilu124@users.noreply.github.com>
2024-07-17 14:23:53 +0300

    done with lab 2

--- 0.6. Advanced Flask fortune-teller/app.py
+++ 0.6. Advanced Flask fortune-teller/app.py
@@ -1,16 +1,23 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request,redirect,url_for
 import random
 
 app = Flask(__name__)
 
-@app.route("/home")
+@app.route("/home", methods={"GET","POST"})
 def home():
-    return render_template("home.html")
+    if request.method == "GET":
+        return render_template("home.html")
+    else:
+        if (len(request.form["month"]) > 9):
+            return redirect(url_for('fortune', month="september"))
+        
+        return redirect(url_for('fortune', month=request.form["month"]))
 
-@app.route("/fortune")
-def fortune():
+@app.route("/fortune/<month>")
+def fortune(month):
     forunes = ["good day", "bad day", "ok day", "your dad will die", "it's your birthday", "you will get a gift", "you will die", "you will get hurt", "you will buy something", "it's your moms birthday"]
-    return render_template("fortune.html", fortune=forunes[random.randint(0,9)])
+    return render_template("fortune.html", fortune=forunes[len(month)-1])
+
 
 if __name__ == '__main__':
     app.run(debug=True)
